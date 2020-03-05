b61cfc1472389f43114a17cf8c8a4a6300679337
Thanh Ngo <510thanh.ngo@gmail.com>
2020-03-05 15:45:08 -0800

    Finished readme generator

--- readmeGen/index.js
+++ readmeGen/index.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
 const inquirer = require("inquirer");
 const fs = require('fs');
-
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 inquirer.prompt([
   {
     type: "list",
@@ -9,7 +10,7 @@ inquirer.prompt([
     name: "choice",
     choices: [
       "Create a README file",
-      "Get github profile"
+      "Generate github profile"
     ]
   }
 ]).then( function(data) {
@@ -19,6 +20,57 @@ inquirer.prompt([
   else
     getGit();
 });
+
+const questions = [
+  {
+    message: "enter your GitHub username:",
+    name: "username"
+  },
+  {
+    message: "Enter your project's title:",
+    name: "title"
+  },
+  {
+    message: "Enter your project's description:",
+    name: "description"
+  },
+  {
+    message: "Enter table of content",
+    name: "toc"
+  },
+  {
+    message: "Enter installation note:",
+    name: "installation"
+  },
+  {
+    message: "Enter usage note:",
+    name: "usage"
+  },
+  {
+    type: "list",
+    message: "Pick a license for your project",
+    name: "license",
+    choices: [
+      "Apache 2.0 License",
+      "BSD 3-Clause License",
+      "Creative Common",
+      "GNU GPL v3",
+      "The MIT License",
+    ]
+  },
+  {
+    message: "Enter contributing note: ",
+    name: "contribute"
+  },
+  {
+    message: "Enter test note:",
+    name: "test"
+  },
+  {
+    message: "Enter questions:",
+    name: "question"
+  }
+];
 
 async function getGit() {
   try {
@@ -31,73 +83,62 @@ async function getGit() {
 
 async function generateReadme() {
   try {
-    console.log("Let's make a README file !\nIn order to do that, please");
-    // Get github username
-    const { username } = await inquirer.prompt({
-      message: "enter your GitHub username:",
-      name: "username"
-    });
+    console.log("Let's make a README file !\n");
+    const answers = await inquirer.prompt(questions);
 
     // Get github's info by API
-    const { data } = await axios.get(`https://api.github.com/users/${username}/events/public`);
+    const { data } = await axios.get(`https://api.github.com/users/${answers.username}/events/public`);
     const avatar_url = data[1].actor.avatar_url;
     const email = data[1].payload.commits[0].author.email;
     const name = data[1].payload.commits[0].author.name;
-    // Get user input for title
-    const { title } = await inquirer.prompt({
-      message: "Enter your project's title:",
-      name: "title"
-    });
 
-    // Set readme title
-    var readme = `# ${title}\n`;
-    // Get user input for description
-    const { description } = await inquirer.prompt({
-      message: "Enter your project's description:",
-      name: "description"
-    });
-    // Append description to readme
-    readme += `## Description\n${description}\n`;
-    // Get user input for TOC
-    const { toc } = await inquirer.prompt({
-      message: "Enter table of content",
-      name: "toc"
-    });
-    // Append toc to readme
-    readme += `## Table of Contents\n${toc}\n`;
-    // Get installation note
-    const { installation } = await inquirer.prompt({
-      message: "Enter installation note:",
-      name: "installation"
-    });
-    // Append installation note
-    readme += `## Installation\n${installation}\n`;
-    // Get usage note
-    const { usage } = await inquirer.prompt({
-      message: "Enter usage note:",
-      name: "usage"
-    });
-    // Append usage note
-    readme += `## Usage\n${usage}\n`;
-    // Licenses
-    readme += `## License\n`;
-    const { license } = await inquirer.prompt({
-      type: "list",
-      message: "Pick a license for your project",
-      name: "license",
-      choices: [
-        "Apache 2.0 License",
-        "BSD 3-Clause License",
-        "Creative Common",
-        "Eclipse Public License 1.0",
-        "GNU GPL v3",
-        "The MIT License",
-      ]
-    });
-      if (license === "Apache 2.0 License")
+    switch (answers.license) {
+      case "Apache 2.0 License":
+        answers.license = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+        break;
+      case "BSD 3-Clause License":
+        answers.license = "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+        break;
+      case "Creative Common":
+        answers.license = "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
+        break;
+      case "GNU GPL v3":
+        answers.license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+        break;
+      case "The MIT License":
+        answers.license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    };
 
+    const readme =
+`# ${answers.title}
 
+## Description
+${answers.description}
 
+## Table of Contents
+${answers.toc}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+
+## Contributing
+${answers.contribute}
+
+## Tests
+${answers.test}
+
+## Questions
+${answers.question}`;
+
+    await writeFileAsync(
+      "README.md",readme
+    );
   } catch (err) {
     console.log(err);
   }
