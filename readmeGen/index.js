41a5e0354f47fcb95787d1dfc8defceb1a69e9c5
Thanh Ngo <510thanh.ngo@gmail.com>
2020-03-07 01:36:57 -0800

    Add function init & repo

--- readmeGen/index.js
+++ readmeGen/index.js
@@ -2,30 +2,14 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 const fs = require('fs');
 const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
-inquirer.prompt([
-  {
-    type: "list",
-    message: "What do you want to do ?",
-    name: "choice",
-    choices: [
-      "Create a README file",
-      "Generate github profile"
-    ]
-  }
-]).then( function(data) {
-  console.log(data);
-  if (data.choice === "Create a README file")
-    generateReadme();
-  else
-    getGit();
-});
+//const writeFileAsync = util.promisify(fs.writeFile);
+
+// Decalre global variables
+var avatar_url, email, name;
+// Initialize to get user's information
+init();
 
 const questions = [
-  {
-    message: "enter your GitHub username:",
-    name: "username"
-  },
   {
     message: "Enter your project's title:",
     name: "title"
@@ -72,25 +56,70 @@ const questions = [
   }
 ];
 
-async function getGit() {
-  try {
+function getRepo(username) {
+  const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
-  } catch (err) {
-    console.log(err);
-  }
+  axios.get(queryUrl).then(function(res) {
+    const repoNames = res.data.map(function(repo) {
+      return repo.name;
+    });
 
+    const repoNamesStr = repoNames.join("\n");
+    console.log(repoNamesStr);
+
+    fs.writeFile("repo.txt", repoNamesStr, function(err) {
+      if (err) {
+        throw err;
+      }
+
+      console.log("Successfully wrote to repo.txt file");
+    });
+
+  });
+}
+
+function init() {
+  inquirer.prompt([
+    {
+      message: "Enter your github username: ",
+      name: "username",
+    },
+    {
+      type: "list",
+      message: "What do you want to do ?",
+      name: "choice",
+      choices: [
+        "Create a README file",
+        "Generate github profile"
+      ]
+    }
+  ]).then( function(data) {
+    // Get user's email, name, avatar
+    const queryUrl = `https://api.github.com/users/${data.username}/events/public`;
+
+    axios.get(queryUrl).then(function(res) {
+      avatar_url = res.data[1].actor.avatar_url;
+      email = res.data[1].payload.commits[0].author.email;
+      name = res.data[1].payload.commits[0].author.name;
+    });
+    if (data.choice === "Create a README file")
+      generateReadme();
+    else
+      getRepo(data.username);
+  });
 }
 
 async function generateReadme() {
   try {
     console.log("Let's make a README file !\n");
     const answers = await inquirer.prompt(questions);
-
+    /*
     // Get github's info by API
     const { data } = await axios.get(`https://api.github.com/users/${answers.username}/events/public`);
     const avatar_url = data[1].actor.avatar_url;
     const email = data[1].payload.commits[0].author.email;
     const name = data[1].payload.commits[0].author.name;
+    */
 
     switch (answers.license) {
       case "Apache 2.0 License":
@@ -136,81 +165,17 @@ ${answers.test}
 ## Questions
 ${answers.question}`;
 
-    await writeFileAsync(
-      "README.md",readme
-    );
+  //  await writeFileAsync(
+  //    "README.md",readme
+  //  );
   } catch (err) {
     console.log(err);
   }
 }
 
-/*const axios = require("axios");
-const inquirer = require("inquirer");
-const fs = require('fs');
-
-console.log("Let's make a README file !\nIn order to do that, please");
-getGit();
-
-async function getGit() {
-  try {
-    const { username } = await inquirer.prompt({
-      message: "Enter your username: ",
-      name: "username"
-    });
-    const { data } = await axios.get(`https://api.github.com/users/${username}/events/public`);
-
-    console.log(data[1].actor.avatar_url); // Avatar URL
-    //console.log(data[1].payload.commits);
-    var git_info = JSON.parse(data[1].payload.commits);
-    console.log(git_info);
-  } catch (err) {
-    //console.log(err);
-  }
-}
-
-inquirer.prompt([
-  {
-    type: "input",
-    name: "username",
-    message: "Enter your GitHub username:"
-  },
-
-  {
-    type: "input",
-    name: "Okay, let's start.\nEnter project title: ",
-    message: "title"
-  },
-  {
-    type: "input",
-    name: "Now, let's make a table of contents for your readme \n Enter a label for your badge: ",
-    message: "badge_label"
-  }
-]);
-
-// Create table of content
-let loop_toc = true;
-while (loop_toc) {
-  inquirer.prompt([
-
-    {
-      type: "list",
-      message: "Do you want to m"
-    }
-  ]).then( answers => {
-
-  });
-}
-
-const questions = [
-
-];
+/*
 
 function writeToFile(fileName, data) {
 }
 
-function init() {
-
-}
-
-init();
 */
