5cd2083f53a4174349b4f672ff73cd2935ae7fed
thanhn <510thanh.ngo@gmail.com>
2020-04-02 03:21:49 -0700

    searchID() added

--- weatherBoard/script.js
+++ weatherBoard/script.js
@@ -1,6 +1,20 @@
-$.ajax({
-  url: "file:///C:/Users/anhtn/Desktop/GitLab/thanhn062.github.io/weatherBoard/us-city-code.json",
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
+$(".btn").press()
+searchID(searchQuery) {
+  $.ajax({
+    url: "https://raw.githubusercontent.com/thanhn062/thanhn062.github.io/master/weatherBoard/us-city-code.json",
+    method: "GET"
+  }).then(function(response) {
+    // search for city's name
+    for (var i = 0; i < response.length; i++) {
+      if (response[i].name.indexOf(searchQuery) >= 0) {
+        var id = response[i].id;
+        showResults(id);
+        break;
+      }
+    }
+  });
+}
+
+showResults(id) {
+  
+}
