5f498b87693901c4503d2b2d005621f50dad80c5
thanhn <510thanh.ngo@gmail.com>
2020-04-06 00:58:52 -0700

    fix semi colon

--- EmployeeTracker/schema.sql
+++ EmployeeTracker/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE department (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE role (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -15,7 +15,7 @@ CREATE TABLE role (
   `salary` DECIMAL NOT NULL,
   `department_id` INT NOT NULL,
   PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE employee (
   `id` INT NOT NULL AUTO_INCREMENT,
