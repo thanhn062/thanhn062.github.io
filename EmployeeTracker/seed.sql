fe62971da378e9512fb2f221d25250cd9e410509
thanhn <510thanh.ngo@gmail.com>
2020-04-06 01:00:11 -0700

    Fix null insert

--- EmployeeTracker/seed.sql
+++ EmployeeTracker/seed.sql
@@ -5,13 +5,13 @@ VALUES ("John","Doe",1,3); -- 1
 INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
 VALUES ("Mike","Chan",2,1); -- 2
 INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
-VALUES ("Ashley","Rodriguez",3,); -- 3
+VALUES ("Ashley","Rodriguez",3,null); -- 3
 INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
 VALUES ("Kevin","Tupik",4,3); -- 4
 INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
-VALUES ("Malia","Brown",5,); -- 5
+VALUES ("Malia","Brown",5,null); -- 5
 INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
-VALUES ("Sarah","Lourd",6,); -- 6
+VALUES ("Sarah","Lourd",6,null); -- 6
 INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
 VALUES ("Tom","Allen",7,6); -- 7
 INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
