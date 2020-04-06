5950a0676510f5542dadffdf7d199d1fad022192
thanhn <510thanh.ngo@gmail.com>
2020-04-06 00:47:05 -0700

    schema.sql adding tables & assigning roles & department it belong to

--- EmployeeTracker/schema.sql
+++ EmployeeTracker/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS `employee_DB`;
 CREATE database `employee_DB`;
 
 USE `employee_DB`;
-
+-- Create tables
 CREATE TABLE department (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(30) NOT NULL,
@@ -25,3 +25,25 @@ CREATE TABLE employee (
   `manager_id` INT,
   PRIMARY KEY (id)
 );
+
+-- Assign department id
+INSERT INTO `department` (name) VALUES ("Sales");
+INSERT INTO `department` (name) VALUES ("Engineering");
+INSERT INTO `department` (name) VALUES ("Finance");
+INSERT INTO `department` (name) VALUES ("Legal");
+
+-- Assign role id & department it belong to
+INSERT INTO `role` (title, salary, department_id)
+VALUES ("Sales Lead",100000,1);
+INSERT INTO `role` (title, salary, department_id)
+VALUES ("Sales person",80000,1);
+INSERT INTO `role` (title, salary, department_id)
+VALUES ("Lead Engineer",150000,2);
+INSERT INTO `role` (title, salary, department_id)
+VALUES ("Software Engineer",120000,2);
+INSERT INTO `role` (title, salary, department_id)
+VALUES ("Accountant",125000,3);
+INSERT INTO `role` (title, salary, department_id)
+VALUES ("Legal Team Lead",250000,4);
+INSERT INTO `role` (title, salary, department_id)
+VALUES ("Lawyer",190000,4);
---
+++ EmployeeTracker/seed.sql
@@ -0,0 +1,28 @@
+USE `employee_DB`;
+
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("John","Doe",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
+INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
+VALUES ("","",,);
