c80a436c843a7e6b955854f73471f32b536a4765
thanhn <510thanh.ngo@gmail.com>
2020-04-06 00:49:40 -0700

    Add role id comment

--- EmployeeTracker/schema.sql
+++ EmployeeTracker/schema.sql
@@ -34,16 +34,16 @@ INSERT INTO `department` (name) VALUES ("Legal"); -- 4
 
 -- Assign role id & department it belong to
 INSERT INTO `role` (title, salary, department_id)
-VALUES ("Sales Lead",100000,1);
+VALUES ("Sales Lead",100000,1); -- 1
 INSERT INTO `role` (title, salary, department_id)
-VALUES ("Sales person",80000,1);
+VALUES ("Sales person",80000,1); -- 2
 INSERT INTO `role` (title, salary, department_id)
-VALUES ("Lead Engineer",150000,2);
+VALUES ("Lead Engineer",150000,2); -- 3
 INSERT INTO `role` (title, salary, department_id)
-VALUES ("Software Engineer",120000,2);
+VALUES ("Software Engineer",120000,2); -- 4
 INSERT INTO `role` (title, salary, department_id)
-VALUES ("Accountant",125000,3);
+VALUES ("Accountant",125000,3); -- 5
 INSERT INTO `role` (title, salary, department_id)
-VALUES ("Legal Team Lead",250000,4);
+VALUES ("Legal Team Lead",250000,4); -- 6
 INSERT INTO `role` (title, salary, department_id)
-VALUES ("Lawyer",190000,4);
+VALUES ("Lawyer",190000,4); -- 7
