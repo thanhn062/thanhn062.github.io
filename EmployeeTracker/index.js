c3abcfaf23c47541b06712873dffee657cf487c7
thanhn <510thanh.ngo@gmail.com>
2020-04-07 05:03:45 -0700

    addEmployee() function

--- EmployeeTracker/index.js
+++ EmployeeTracker/index.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-const util = require("util");
+
+
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -44,8 +45,9 @@ function viewMenu() {
       case "View Roles":
         viewRoles();
         break;
-      default:
-
+      case "Add Employee":
+        addEmployee();
+        break;
       }
   });
 }
@@ -86,9 +88,92 @@ function viewRoles() {
     viewMenu();
   });
 }
-function addEmployee() {
-
+async function addEmployee() {
+  var { first_name } = await inquirer.prompt({
+    message: "Enter employee's first name:",
+    name: "first_name"
+  });
+  console.log(first_name);
+  var { last_name } = await inquirer.prompt({
+    message: "Enter employee's last name:",
+    name: "last_name"
+  });
+  console.log(last_name);
+  var { role } = await inquirer.prompt({
+    message: "Pick employee's role:",
+    name: "role",
+    type: "list",
+    choices: [
+      "Sales person",
+      "Sales Lead",
+      "Software Engineer",
+      "Accountant",
+      "Lead Engineer",
+      "Lawyer",
+      "Legal Team Lead"
+    ]
+  });
+  console.log(role);
+  // Convert choices into ID
+  var role_id = 0;
+  if (role == "Sales Lead") role_id = 1;
+  else if (role == "Sales person") role_id = 2;
+  else if (role == "Lead Engineer") role_id = 3;
+  else if (role == "Software Engineer") role_id = 4;
+  else if (role == "Accountant") role_id = 5;
+  else if (role == "Legal Team Lead") role_id = 6;
+  else if (role == "Lawyer") role_id = 7;
+  console.log(role_id);
+  // Get manager id
+  var { manager_id } = await inquirer.prompt({
+    message: "Enter employee's manager's ID:",
+    type: "input",
+    name: "manager_id"
+  });
+  if (!manager_id)
+    manager_id = "null";
+  console.log(manager_id);
+  // Run SQL query to add the employee
+  connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${first_name}","${last_name}",${role_id},${manager_id})`, (err, res) => {
+    if (err) throw err;
+      console.log("Employee added !")
+    viewMenu();
+  });
 }
+
+
+
+/*
+
+
+
+ function getEmpInfo() {
+    // Get Employee's name
+     inquirer.prompt([
+       {
+         message: "Enter employee's first name:",
+         name: "first_name"
+       },
+       {
+         message: "Enter employee's last name:",
+         name: "last_name"
+       },
+       {
+         message: "Employee's department:",
+         name: "department",
+         type: "list",
+         choices: [
+           "Sales",
+           "Engineering",
+           "Finance",
+           "Legal"
+         ]
+       }
+     ]);
+  }
+/*
+console.log(department);
+console.log(name);
 function addRole() {
 
 }
@@ -96,5 +181,18 @@ function addDepartment() {
 
 }
 function updateEmpRole() {
-  
+
 }
+
+// Pick department
+
+
+      var { manager_id } = await
+        inquirer.prompt({
+          message: "Employee's manager ID:",
+          name: "manager_id",
+          type: "input"
+        });
+        return name, role_id, manager_id;
+}
+*/
