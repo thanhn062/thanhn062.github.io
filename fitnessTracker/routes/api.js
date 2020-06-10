b617b7fd9bde282fb93f5a33a710abca837b28f0
thanhn <510thanh.ngo@gmail.com>
2020-06-10 13:45:27 -0700

    update

--- fitnessTracker/routes/api.js
+++ fitnessTracker/routes/api.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 // const Transaction = require("../models/transaction.js");
-var path = require("path");
 const Workout = require("../models/workout.js");
 
-router.post("/exercise", ({ body }, res) => {
-  Workout.create(body)
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
@@ -12,6 +11,14 @@ router.post("/exercise", ({ body }, res) => {
       res.status(400).json(err);
     });
 });
-
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
--- fitnessTracker/server.js
+++ fitnessTracker/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const PORT = process.env.PORT || 3000
 
 const app = express();
