4cdfe87c5b7f967ea58c18844660131e49ae6666
thanhn <510thanh.ngo@gmail.com>
2020-06-11 01:50:52 -0700

    Update new app into portfolio

--- PWA/server.js
+++ PWA/server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
@@ -24,5 +24,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  if (process.env.PORT)
+    console.log(`App running on port ${process.env.PORT}!`);
+  else
+    console.log(`App running on port ${PORT}!`);
 });
